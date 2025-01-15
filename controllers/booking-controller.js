03edc5e3ed13e17d68b6321a8e2e22fa8a1e9db3
kavita <kavitasoren2000@gmail.com>
2025-01-15 23:43:44 +0530

    commit

---
+++ controllers/booking-controller.js
@@ -0,0 +1,77 @@
+import mongoose from "mongoose";
+import Bookings from "../models/Bookings.js";
+import Movie from "../models/Movie.js";
+import User from "../models/User.js";
+
+export const newBooking = async (req, res, next) => {
+  const { movie, date, seatNumber, user } = req.body;
+
+  let existingMovie;
+  let existingUser;
+  try {
+    existingMovie = await Movie.findById(movie);
+    existingUser = await User.findById(user);
+  } catch (err) {
+    return console.log(err);
+  }
+  if (!existingMovie) {
+    return res.status(404).json({ message: "Movie Not Found With Given ID" });
+  }
+  if (!user) {
+    return res.status(404).json({ message: "User not found with given ID " });
+  }
+  let booking;
+
+  try {
+    booking = new Bookings({
+      movie,
+      date: new Date(`${date}`),
+      seatNumber,
+      user,
+    });
+    existingUser.bookings.push(booking);
+    existingMovie.bookings.push(booking);
+    await existingUser.save();
+    await existingMovie.save();
+    await booking.save();
+  } catch (err) {
+    return console.log(err);
+  }
+
+  if (!booking) {
+    return res.status(500).json({ message: "Unable to create a booking" });
+  }
+
+  return res.status(201).json({ booking });
+};
+
+export const getBookingById = async (req, res, next) => {
+  const id = req.params.id;
+  let booking;
+  try {
+    booking = await Bookings.findById(id);
+  } catch (err) {
+    return console.log(err);
+  }
+  if (!booking) {
+    return res.status(500).json({ message: "Unexpected Error" });
+  }
+  return res.status(200).json({ booking });
+};
+
+export const deleteBooking = async (req, res, next) => {
+  const id = req.params.id;
+  let booking;
+  try {
+    booking = await Bookings.findByIdAndDelete(id);
+    return res.status(200).json({
+      booking,
+      message : "Successfully Deleted"
+    })
+  } catch (err) {
+    return console.log(err);
+  }
+  if (!booking) {
+    return res.status(500).json({ message: "Unable to Delete" });
+  }
+};
